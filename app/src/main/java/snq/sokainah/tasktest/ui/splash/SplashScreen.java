0a8e441dabb84814929a3e054f2973f98b1b9644
Sokainahalqawaz <sokaina155@hotmail.com>
2021-10-18 12:33:35 +0300

    Initial commit

---
+++ app/src/main/java/snq/sokainah/tasktest/ui/splash/SplashScreen.java
@@ -0,0 +1,16 @@
+package snq.sokainah.tasktest.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import snq.sokainah.tasktest.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
